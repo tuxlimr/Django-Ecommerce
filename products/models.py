197e75ba69f44f1c1903e0ee082cd00714f3a5d6
ajay <ajay.simplifyreality@gmail.com>
2020-02-09 20:26:01 +0530

    first Model Created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=150,unique=True)
+    description = models.TextField(null=True, blank=True)
+    price  = models.DecimalField(max_digits=1000, decimal_places= 20)
+    slug = models.SlugField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active  = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
