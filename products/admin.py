dc8442ca2b65aa0ee2753aa19a3094b617e2ac00
ajay <ajay.simplifyreality@gmail.com>
2020-02-09 20:57:06 +0530

    Django Enrich Admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    list_display= ['title','__str__','price','timestamp']
+    list_editable = ['price']
+    list_filter =  ['price', 'active']
+    readonly_fields = ['update','timestamp']
+    search_fields= ['title','price','timestamp','description']
+    prepopulated_fields = {"slug" :('title',)}
+    
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
 
 
 
--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=150,unique=True)
     description = models.TextField(null=True, blank=True)
-    price  = models.DecimalField(max_digits=1000, decimal_places= 20)
+    price  = models.DecimalField(max_digits=1000, decimal_places= 2)
     slug = models.SlugField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     update = models.DateTimeField(auto_now_add=False, auto_now=True)
